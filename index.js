6dfaaedbad2cbea78b06c7132eb71d8edbdd307e
niftylettuce <niftylettuce@gmail.com>
2019-12-15 23:03:52 -0600

    fix: added silent flag

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const filePath = path.join(__dirname, 'spfcheck2.py');
 // ensure python installed
 if (!which('python3')) throw new Error(`Python v3.5+ is required`);
 
+const silent = process.env.NODE_ENV !== 'test';
+
 // ensure python v3.5+
-let version = exec('python3 --version');
+let version = exec('python3 --version', { silent });
 version = semver.coerce(
   (version.stdout || version.stderr).split(' ')[1].trim()
 );
@@ -22,6 +24,7 @@ module.exports = function(ip, address, host) {
   return new Promise((resolve, reject) => {
     exec(
       `python3 ${filePath} ${ip} ${address} ${host}`,
+      { silent },
       (code, stdout, stderr) => {
         if (code !== 0) return reject(new Error(stderr));
         resolve(stdout.trim().split(','));
